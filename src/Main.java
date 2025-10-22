186a7dc87b20c263657708ee3c4cc31164f33893
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2025-10-22 11:19:00 +0300

    window start & login page as first page

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import pages.LoginPage;
+
 public class Main {
     public static void main(String[] args) {
-        // main code here
+        /* Check usage of MainWindow */
+        /* Adds login page and loads it as the first page */
+        MainWindow.addPage("login", new LoginPage());
+        MainWindow.goTo("login");
+        MainWindow.start();
     }
 }
